3b1fab7b17f95f736bcd4a7ef5aea321d071c0ba
Tatsu821 <tatsuhiro821@outlook.jp>
2024-10-05 19:40:52 +0900

    first-commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// app.js
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// 静的ファイルの提供
+app.use(express.static(path.join(__dirname, 'public')));
+
+// "/" (ホームページ) のルート設定
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/home.html'));
+});
+
+// "/(user_id)/mypage" のルーティング設定
+const mypageRouter = require('./routes/mypage');
+app.use('/', mypageRouter);
+
+// サーバーの起動
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
---
+++ routes/mypage.js
@@ -0,0 +1,13 @@
+// routes/mypage.js
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+// ユーザーIDを受け取ってmypage.htmlを返す
+router.get('/:user_id/mypage', (req, res) => {
+    const userId = req.params.user_id;
+    // 静的ファイルを返す (user_idを使って動的処理を行う場合はEJSなどを使う)
+    res.sendFile(path.join(__dirname, '../public/mypage.html'));
+});
+
+module.exports = router;
