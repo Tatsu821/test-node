fc528bbaca3777f1472bc3dbb36cfcc07d51c229
Tatsu821 <tatsuhiro821@outlook.jp>
2024-10-05 20:45:06 +0900

    router

---
+++ docs/js/route.js
@@ -0,0 +1,17 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const path = window.location.pathname;
+
+    // ユーザーIDに基づいてmypageを表示する場合
+    if (path.match(/^\/\d+\/mypage\/?$/)) {
+        const userId = path.split('/')[1];
+        fetch('/pages/mypage.html')
+            .then(response => response.text())
+            .then(html => {
+                document.getElementById('content').innerHTML = html;
+
+                // ユーザーIDを表示する
+                document.getElementById('userId').textContent = userId;
+            });
+    }
+});
