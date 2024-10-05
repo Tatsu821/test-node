2d10100408485e9c1f0fee1df632e13ce901a10f
Tatsu821 <tatsuhiro821@outlook.jp>
2024-10-05 20:51:54 +0900

    fix

--- docs/js/route.js
+++ docs/js/route.js
@@ -2,10 +2,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     const path = window.location.pathname;
 
-    // ユーザーIDに基づいてmypageを表示する場合
-    if (path.match(/^\/\d+\/mypage\/?$/)) {
-        const userId = path.split('/')[1];
-        fetch('/docs/mypage.html')
+    // test-node/(user_id)/mypage のURLに対応する
+    if (path.match(/^\/test-node\/\d+\/mypage\/?$/)) {
+        const userId = path.split('/')[2];  // test-node の後の user_id を取得
+        fetch('/test-node/mypage.html')  // test-node内のmypage.htmlを読み込む
             .then(response => response.text())
             .then(html => {
                 document.getElementById('content').innerHTML = html;
